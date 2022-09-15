b28b225649b2433199ba493f6ef72b5b9fbdbce8
JVCAv1 <joao.assumpo@gmail.com>
2022-09-15 19:23:49 -0300

    All Done!

---
+++ clippy/target/debug/deps/clippy3-1f40565d19d5999c.d
@@ -0,0 +1,7 @@
+/stg/sta/Projects/rustlings/exercises/clippy/target/debug/deps/clippy3-1f40565d19d5999c.rmeta: clippy3.rs
+
+/stg/sta/Projects/rustlings/exercises/clippy/target/debug/deps/clippy3-1f40565d19d5999c.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
